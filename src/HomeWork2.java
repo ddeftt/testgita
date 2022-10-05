471209c671c96d1d37b01681112967490692b650
Tester oprogramowania <testersoftu@gmail.com>
2022-10-05 22:42:00 +0200

    Java staff

--- src/HomeWork2.java
+++ src/HomeWork2.java
@@ -5,10 +5,12 @@ public class HomeWork2 {
         System.out.println("Powiedz prosze ile masz lat? - ");
         Scanner scanner = (new Scanner(System.in));
         int age = scanner.nextInt();
-        if (age < 18) {
+        if (age < 18 && age > 0) {
             System.out.println("jeśli masz " + age + " lat to nie moge Ci sprzedać akoholu");
         } else if (age >= 18) {
             System.out.println("Dziękuje za zakupy");
+        } else if (age <= 0) {
+            System.out.println("Podaj prosze poprawny wiek");
         }
     }
 }
