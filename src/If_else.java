eb7440352aaffbe6a1a0eca147f730724f9ba72a
Tester oprogramowania <testersoftu@gmail.com>
2022-10-30 23:15:01 +0100

    test

---
+++ src/If_else.java
@@ -0,0 +1,12 @@
+public class If_else {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 5000;
+        int levelBonus = 5;
+        int bonus = 100;
+
+        if(score == 5000) {
+            System.out.println("Your score is 5k");
+        }
+    }
+}
